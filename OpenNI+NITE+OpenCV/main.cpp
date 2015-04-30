2388737152545ed220aa55a8015bfc7523427317
Rishad Bharucha <rb2102@gmail.com>
2015-04-30 14:17:11 -0600

    Initial Commit

---
+++ OpenNI+NITE+OpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenNI+NITE+OpenCV
+//
+//  Created by Rishad Bharucha on 4/30/15.
+//  Copyright (c) 2015 Rishad Bharucha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
